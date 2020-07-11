d182e6cd40620dab8309fc8c790a8c3df5d0ee72
danish45007 <myemail>
2020-07-11 20:57:55 +0530

    all cors allowed

--- app/main.py
+++ app/main.py
@@ -18,14 +18,7 @@ app = FastAPI()
 
 
 # cors added
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:3000",
-
-]
+origins = ['*']
 
 
 app.add_middleware(
